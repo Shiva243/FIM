4e1340bd12a86f0f979813035695b61c443aa58b
Shivakoti <shivav809@gmail.com>
2017-02-22 18:32:49 +0530

    Update Jenkinsfile.groovy

--- FIM/Jenkinsfile.groovy
+++ FIM/Jenkinsfile.groovy
@@ -1,32 +1,5 @@
-pipeline {
-    node{
+def devStaging() {
+    stage 'Dev'{
         echo 'test';
-    agent any
-    parameters {
-        string (
-            defaultValue: '*',
-            description: '',
-            name : 'BRANCH_PATTERN')
-        booleanParam (
-            defaultValue: false,
-            description: '',
-            name : 'FORCE_FULL_BUILD')
-    }
-
-    stages {
-        stage ('Pullrequest for code') {
-            steps {
-                checkout([$class: 'GitSCM',
-                    branches: [[name: "origin/${BRANCH_PATTERN}"]],
-                    doGenerateSubmoduleConfigurations: false,
-                    extensions: [[$class: 'LocalBranch']],
-                    submoduleCfg: [],
-                    userRemoteConfigs: [[
-                        credentialsId: 'a1a2265a-6c99-43cd-941d-62ee5b4b10e1',
-                        url: 'https://github.com/Shiva243/FIM.git']]])
-            }
-        }
-   
-    }
     }
 }
